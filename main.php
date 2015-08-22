88e4d8493092e2857937f98fe6c759634fe5ed53
naa986 <naa986@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-22 06:25:32 +0000

    .committing version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/simple-universal-google-analytics/trunk@1227813 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: Simple Universal Google Analytics
-  Version: 1.0.1
+  Version: 1.0.2
   Plugin URI: http://wphowto.net/simple-universal-google-analytics-plugin-for-wordpress-822
   Author: naa986
   Author URI: http://wphowto.net/
@@ -15,7 +15,7 @@ if (!class_exists('SIMPLE_UNIVERSAL_GA')) {
 
     class SIMPLE_UNIVERSAL_GA {
 
-        var $plugin_version = '1.0.1';
+        var $plugin_version = '1.0.2';
 
         function __construct() {
             define('SIMPLE_UNIVERSAL_GA_VERSION', $this->plugin_version);
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: naa986
 Donate link: http://wphowto.net/
 Tags: analytics, google, google analytics, statistics, stats, tracking 
 Requires at least: 4.2
-Tested up to: 4.2
-Stable tag: 1.0.1
+Tested up to: 4.3
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -56,6 +56,10 @@ Yes.
 none
 
 == Changelog ==
+
+= 1.0.2 =
+
+* Simple Universal Google Analytics is now compatible with WordPress 4.3
 
 = 1.0.1 =
 
